26bf593576efe86771984e047cdd629b701cfcb7
theo82 <theoaristi53@gmail.com>
2021-01-25 11:31:53 +0200

    Setting Up a Navigator

---
+++ navigation/ShopNavigator.js
@@ -0,0 +1,19 @@
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import { Platform } from 'react-native';
+
+import ProductsOverViewScreen from '../screens/shop/ProductOverviewScreen';
+import Colors from '../constants/Colors';
+
+const ProductsNavigator = createStackNavigator({
+    ProductsOverview: ProductsOverViewScreen
+}, {
+    defaultNavigationOptions: {
+        headerStyle: {
+            backgroundColor: Platform.OS === 'android' ? Colors.primary : ''
+        },
+        headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primary
+    }
+})
+
+export default createAppContainer(ProductsNavigator)
