221ac35b273e9e0dd7d37914b8680a7b7745a1e2
theo82 <theoaristi53@gmail.com>
2021-01-26 09:01:48 +0200

    Using Custom Fonts

--- App.js
+++ App.js
@@ -6,6 +6,9 @@ import { Provider } from 'react-redux';
 import productsReducer from './store/reducers/products';
 import ShopNavigator from './navigation/ShopNavigator';
 
+import { useFonts } from 'expo-font';
+import AppLoading from 'expo-app-loading';
+
 const rootReducer = combineReducers({
   products: productsReducer
 });
@@ -13,6 +16,18 @@ const rootReducer = combineReducers({
 const store = createStore(rootReducer);
 
 export default function App() {
+
+  let [fontsLoaded] = useFonts({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
+  })
+
+  if (!fontsLoaded) {
+    return (
+      <AppLoading/>
+    );
+  }
+
   return (
     <Provider store={store}>
         <ShopNavigator />
