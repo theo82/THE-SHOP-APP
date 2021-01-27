5b4619a53dfcfd5c72aab64813d9525d1432cd7a
theo82 <theoaristi53@gmail.com>
2021-01-27 12:57:15 +0200

    Adding Logic to Delete Items

--- screens/shop/CartScreen.js
+++ screens/shop/CartScreen.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { View, Text, FlatList, StyleSheet, Button } from 'react-native';
 import Colors from '../../constants/Colors';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import CartItem from '../../components/shop/CartItem';
+import * as cartActions from '../../store/actions/cart';
 
 const CartScreen = props => {
 
@@ -19,8 +20,11 @@ const CartScreen = props => {
                sum: state.cart.items[key].sum
            }) 
         }
-        return transformedCartItems;
+        return transformedCartItems.sort(
+            (a, b) => a.productId ? 1 : -1
+        );
     });
+    const dispatch = useDispatch();
     return (
         <View style={styles.screen}>
             <View style={styles.summary}>
@@ -42,7 +46,7 @@ const CartScreen = props => {
                         title={itemData.item.productTitle} 
                         amount={itemData.item.sum}
                         onRemove={() => {
-
+                            dispatch(cartActions.removeFromCart(itemData.item.productId));
                         }}
                     /> } 
             />
