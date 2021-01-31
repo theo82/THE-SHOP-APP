05c2ea3443a80471d889d799e34b51ffccbf9471
theo82 <theoaristi53@gmail.com>
2021-01-31 16:58:43 +0200

    Styling Order Items

--- screens/shop/OrdersScreen.js
+++ screens/shop/OrdersScreen.js
@@ -5,29 +5,32 @@ import { useSelector } from 'react-redux';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import HeaderButton from '../../components/UI/HeaderButton';
 
+import OrderItem from '../../components/shop/OrderItem';
+
 const OrdersScreen = props => {
-    const orders = useSelector(state => state.orders.orders)
+    const orders = useSelector(state => state.orders.orders);
 
     return (
         <FlatList 
-            data={orders} 
-            keyExtractor={item => item.id} 
-            renderItem={itemData => <Text>{itemData.item.totalAmount}</Text>} 
+        data={orders} 
+        keyExtractor={item => item.id} 
+        renderItem={itemData => (
+            <OrderItem 
+                amount={itemData.item.totalAmount} 
+                date={itemData.item.readableDate} 
+                />
+            )} 
         />
-    );
+    )
 };
 
 OrdersScreen.navigationOptions = navData => {
     return {
         headerTitle: 'Your Orders',
         headerLeft: () => <HeaderButtons HeaderButtonComponent={HeaderButton}>
-            <Item 
-                title='Menu' 
-                iconName={Platform.OS === 'android' ? 'md-menu' : 'ios-menu'} 
-                onPress={() => {
-                    navData.navigation.toggleDrawer();
-                }}
-            />
+            <Item title='MEun' iconName={Platform.OS === 'android' ? 'md-menu' : 'ios-menu'} onPress={() => {
+                navData.navigation.toggleDrawer();
+            }}/>  
         </HeaderButtons>
     }
 }
