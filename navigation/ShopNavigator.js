d5e82fc10a928379ea105408abbca7b44f041f1b
theo82 <theoaristi53@gmail.com>
2021-02-01 10:04:08 +0200

    Building the User Products Screen

--- navigation/ShopNavigator.js
+++ navigation/ShopNavigator.js
@@ -10,6 +10,7 @@ import Colors from '../constants/Colors';
 import CartScreen from '../screens/shop/CartScreen';
 import OrdersScreen from '../screens/shop/OrdersScreen';
 import { Ionicons } from '@expo/vector-icons';
+import UserProductsScreen from '../screens/user/UserProductsScreen';
 
 const defaultNavOptions = {
     headerStyle: {
@@ -60,13 +61,33 @@ const OrdersNavigator = createStackNavigator({
 }
 );
 
+const AdminNavigator = createStackNavigator({
+  UserProducts: UserProductsScreen,
+
+},{
+  navigationOptions: {
+    drawerIcon: drawerConfig => (
+      <Ionicons
+        name={Platform.OS === 'android' ? 'md-create' : 'ios-create'}
+        size={23}
+        color={drawerConfig.tintColor}
+      />
+    )
+  },
+  defaultNavigationOptions: defaultNavOptions
+}
+);
+
 const ShopNavigator = createDrawerNavigator({
     Products: ProductsNavigator,
-    Orders: OrdersNavigator
+    Orders: OrdersNavigator,
+    Admin: AdminNavigator
 }, {
     contentOptions: {
         activeTintColor: Colors.primary
     }
 })
+
+
 
 export default createAppContainer(ShopNavigator);
