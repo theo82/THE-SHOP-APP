c53a41229926c0dcc4d230897369c42cf4f2cd09
theo82 <theoaristi53@gmail.com>
2021-02-15 19:08:23 +0200

    Managing the Loading State & Errors

--- screens/user/AuthScreen.js
+++ screens/user/AuthScreen.js
@@ -1,5 +1,13 @@
-import React, { useState, useReducer, useCallback } from 'react';
-import { ScrollView, StyleSheet, View, KeyboardAvoidingView, Button } from 'react-native';
+import React, { useState, useEffect, useReducer, useCallback } from 'react';
+import { 
+  ScrollView, 
+  StyleSheet, 
+  View, 
+  KeyboardAvoidingView, 
+  Button, 
+  ActivityIndicator, 
+  Alert 
+} from 'react-native';
 
 import Input from '../../components/UI/Input';
 import Card from "../../components/UI/Card";
@@ -35,6 +43,8 @@ const formReducer = (state, action) => {
   }
 
 const AuthScreen = props => {
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState();
     const [isSignup, setIsSignup] = useState(false);
     const dispatch = useDispatch();
 
@@ -50,7 +60,13 @@ const AuthScreen = props => {
         formIsValid: false
       });
 
-    const authHandler = () => {
+    useEffect(() => {
+      if (error) {
+        Alert.alert('An Error Occurred!', error, [{ text: 'Okay' }]);
+      }
+    }, [error]);
+
+    const authHandler = async () => {
         let action;
         if(isSignup) {
           action = authActions.signup(
@@ -63,7 +79,14 @@ const AuthScreen = props => {
             formState.inputValues.password
           )
         }
-        dispatch(action);
+        setError(null);
+        setIsLoading(true);
+        try{
+          await dispatch(action);
+        }catch(err) {
+          setError(err.message);
+        }
+        setIsLoading(false);
     };
 
     const inputChangeHandler = useCallback(
@@ -110,11 +133,13 @@ const AuthScreen = props => {
                         initialValue=""
                     />
                 <View style={styles.buttonContainer}>
-                  <Button
-                    title={isSignup ? 'Sign Up' : 'Login'}
-                    color={Colors.primary}
-                    onPress={authHandler}
-                  />
+                  {isLoading ? ( <ActivityIndicator size="small" color={Colors.primary} /> 
+                    ) : ( 
+                    <Button 
+                      title={isSignup ? 'Sign Up' : 'Login' }
+                      color={Colors.primary}
+                      onPress={authHandler}
+                  />)}
                 </View>
                 <View style={styles.buttonContainer}>
                   <Button
--- store/actions/auth.js
+++ store/actions/auth.js
@@ -1,30 +1,38 @@
 export const SIGNUP = 'SIGNUP';
 export const LOGIN = 'LOGIN';
 
-export const signup = (email,password) => {
+export const signup = (email, password) => {
     return async dispatch => {
-        const response = fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDaRQrMMWbMZVFuzfpDfgmYuEFmzB7vCOM',
+      const response = await fetch(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDaRQrMMWbMZVFuzfpDfgmYuEFmzB7vCOM',
         {
-            method: 'POST',
-            headers: {
-                'Content-Type':'application/json'
-            },
-            body: JSON.stringify({
-                email: email,
-                password: password,
-                returnSecureToken: true
-            })
-        });
-
-        if(!response.ok) {
-            throw new Error('Something went wrong!');
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            email: email,
+            password: password,
+            returnSecureToken: true
+          })
         }
-
-        const resData = await response.json();
-        console.log(resData);
-        dispatch({ type: SIGNUP });
-    }
-}
+      );
+  
+      if (!response.ok) {
+          const errorResData = await response.json();
+          const errorId = errorResData.error.message;
+          let message = 'Something went wrong!';
+          if (errorId === 'EMAIL_EXISTS') {
+            message = 'This email exists already!';
+          }
+          throw new Error(message);
+        }
+  
+      const resData = await response.json();
+      console.log(resData);
+      dispatch({ type: SIGNUP });
+    };
+  };
 
 export const login = (email, password) => {
     return async dispatch => {
@@ -44,7 +52,18 @@ export const login = (email, password) => {
       );
   
       if (!response.ok) {
-        throw new Error('Something went wrong!');
+        const errorResData = await response.json();
+        const errorId = errorResData.error.message;
+        let message = 'Something went wrong!';
+
+        if(errorId === 'EMAIL_NOT_FOUND') {
+            message = 'This email could not be found';
+        } else if (errorId === 'INVALID_PASSWORD') {
+            message = 'This password is not valid';
+        }
+
+        throw new Error(message);
+
       }
   
       const resData = await response.json();
