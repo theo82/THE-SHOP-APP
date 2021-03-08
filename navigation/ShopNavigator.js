c47f3c0ddf80bfc3b9061c7877d8f01ed2cf4511
theo82 <theoaristi53@gmail.com>
2021-03-08 13:07:54 +0200

    Remaining Migration Steps & Roundup

--- navigation/ShopNavigator.js
+++ navigation/ShopNavigator.js
@@ -57,7 +57,7 @@ export const ProductsNavigator = () => {
         options={productsOverviewScreenOptions}
       />
       <ProductsStackNavigator.Screen
-        name="ProductDetail"
+        name="ProductDetails"
         component={ProductDetailScreen}
         options={productDetailScreenOptions}
       />
